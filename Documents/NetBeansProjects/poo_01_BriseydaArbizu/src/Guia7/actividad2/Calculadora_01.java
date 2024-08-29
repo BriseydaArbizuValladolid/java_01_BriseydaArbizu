8d196047f38a9f8884ec44b24764eec944366b87
Briseyda Arbizu <briseyda.arbizu.valladolid@vallegrande.edu.pe>
2024-08-28 22:07:10 -0500

    actualización del avance

--- Documents/NetBeansProjects/poo_01_BriseydaArbizu/src/Guia7/actividad2/Calculadora_01.java
+++ Documents/NetBeansProjects/poo_01_BriseydaArbizu/src/Guia7/actividad2/Calculadora_01.java
@@ -1,14 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Guia7.actividad2;
 
-/**
- *
- * @author lenovo
- */
 public class Calculadora_01 {
-    
+
+    public double result;
+
+    public void calcularSuma(double numeroUno, double numeroDos) {
+        result = numeroUno + numeroDos;
+    }
+
+    public void calcularResta(double numeroUno, double numeroDos) {
+        result = numeroUno - numeroDos;
+    }
+
+    public void calcularMult(double numeroUno, double numeroDos) {
+        result = numeroUno * numeroDos;
+    }
+
+    public void calcularDiv(double numeroUno, double numeroDos) {
+        result = numeroUno / numeroDos;
+    }
 }
+
